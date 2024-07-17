14cbceb7b6ae4f619762cf16d503d3a3a82cedf7
= <efgeen1998@gmail.com>
2024-07-17 09:54:51 +0000

    .

--- setup.sh
+++ setup.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
+# openssh
+pacman -S --needed --noconfirm openssh
+if ! systemctl is-enabled --quiet sshd; then
+    systemctl enable sshd
+fi
+if ! systemctl is-active --quiet sshd; then
+    systemctl start sshd
+fi
+
+# submodules
 pacman -S --needed --noconfirm git
-
 git submodule update --init
-
 git submodule foreach "
     if [ -f setup.sh ]; then
         sudo sh setup.sh

afb518e4290258a6d3800f180554f6d10dcb61d7
Efgeen <efgeen1998@gmail.com>
2024-07-20 23:24:04 +0200

    .

--- setup.sh
+++ setup.sh
@@ -1,27 +1,54 @@
 #!/bin/sh
 
-# pacman
-pacman -Syu
+f=false
 
-# openssh
-pacman -S --needed --noconfirm openssh
+while getopts ":f" opt; do
+    case $opt in
+	f)
+	    f=true
+	    ;;
+	\?)
+	    ;;
+    esac
+done
+
+if ! pacman -Syu --noconfirm > /dev/null; then
+    echo "[dotfiles] : fail (pacman -Syu)"
+    exit 1
+fi
+
+if ! pacman -S --needed --noconfirm openssh > /dev/null 2>&1; then
+    echo "[dotfiles] : fail (pacman -S openssh)"
+    exit 2
+fi
+
 if ! systemctl is-enabled --quiet sshd; then
     systemctl enable sshd
 fi
+
 if ! systemctl is-active --quiet sshd; then
     systemctl start sshd
 fi
 
-# submodules
-pacman -S --needed --noconfirm git
-git submodule update --init
-git submodule foreach "
-    if [ -f setup.sh ]; then
-        if [ ! -x setup.sh ]; then
-            chmod +x setup.sh
-        fi
-        ./setup.sh
-    fi
-"
+if ! pacman -S --needed --noconfirm git > /dev/null 2>&1; then
+    echo "[dotfiles] : fail (pacman -S git)"
+    exit 3
+fi
 
-echo "[.dotfiles] : success"
+git submodule update --init
+
+opts=""
+
+if [ "$f" = true ]; then
+    opts="${opts} -f"
+fi
+
+cd tmux
+sh setup.sh $opts
+cd - > /dev/null
+
+cd nvim
+# sh setup.sh
+cd - > /dev/null
+
+echo "[dotfiles] : success"

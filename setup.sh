6f9d81eea39ab2810b7a6b3d965fbd736c23bf39
Efgeen <efgeen1998@gmail.com>
2024-08-27 07:15:07 +0200

    .

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+pacman -Syu
+
+# ssh
+pacman -S --noconfirm openssh
+if ! systemctl is-enabled sshd; then
+    systemctl enable sshd
+fi
+if ! systemctl is-active sshd; then
+    systemctl start sshd
+fi
+
+# git
+pacman -S --noconfirm git
+
+# submodules
+git submodule update --init
+git submodule foreach '
+    cd "$sm_path"
+    if [ -f setup.sh ]; then
+        sh setup.sh
+    fi
+    cd -
+'
+

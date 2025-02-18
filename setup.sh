464330b9f5e3ce71302da1f043646a1ad3b42f70
Efgeen <efgeen1998@gmail.com>
2025-02-18 16:06:51 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,20 +1,9 @@
 #!/bin/sh
 
-echo -e "\033[0;32m[.dotfiles] : setup...\033[0m"
+echo -e "\033[0;32m[.dotfiles]\033[0m"
 
-# syu
 pacman -Syu --noconfirm --needed
 
-# ssh
-pacman -S --noconfirm --needed openssh
-if ! systemctl is-enabled sshd; then
-    systemctl enable sshd
-fi
-if ! systemctl is-active sshd; then
-    systemctl start sshd
-fi
-
-# git
 pacman -S --noconfirm --needed git
 git submodule update --init
 git submodule foreach '
